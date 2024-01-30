40775117f25f83737a05ddfa56b981b52ff1d1b9
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-01-30 21:00:19 +0100

    seeders update + id discounts deleted from migration

---
+++ database/seeders/CartsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cart;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CartsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Cart::create([
+            'quantity' => 1,
+        ]);
+        Cart::create([
+            'quantity' => 5,
+        ]);
+    }
+}
