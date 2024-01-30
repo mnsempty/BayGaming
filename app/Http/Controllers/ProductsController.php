831c4b55578f138fe3d8aa04081a1357c5ca42ce
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-01-30 14:28:49 +0100

    test

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller
 {
-    //
+    // Create Read Update Delete
+    // crear producto mediante una transacción
+    // 
+    public function crearProduct(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['message' => 'Error al crear un producto']);
+        }
+    }
 }
