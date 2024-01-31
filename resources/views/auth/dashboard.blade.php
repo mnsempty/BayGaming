94f80757e9e2b59c5373d59b19b94fb3d716f21e
SeidaSuarez <115797031+SeidaSuarez@users.noreply.github.com>
2024-01-31 21:52:10 +0100

    *

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -47,25 +47,11 @@
                             <a class="btn btn-info" href="">Show</a>
                             {{-- {{ route('products.edit',$product->id) }} --}}
                             <a class="btn btn-primary" href="">Edit</a>
-
-        @foreach ($products as $product)
-            <tr>
-                <td>{{ $product->name }}</td>
-                <td>{{ $product->price }}</td>
-                <td>{{ $product->developer }}</td>
-                <td>{{ $product->platform }}</td>
-                <td>
-                    <form action="{{ route('product.delete',$product->id) }}" method="POST">
-                        <a class="btn btn-info" href="{{ route('home',$product->id) }}">Show</a>
-                        <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
-                </td>
-                </td>
-            </tr>
-        @endforeach
+                        </form>
+                    </td>
+                    </td>
+                </tr>
+            @endforeach
         @endif
     </table>
 @endsection
